e6986708e92a5db73084b286b798bb6bd63377e0
bli4918 <bwli266@gmail.com>
2021-09-25 23:48:10 -0400

    Update

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Bo on 9/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+        
+    @IBOutlet weak var posterView: UIImageView!
+}
